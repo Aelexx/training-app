e7d6fe3882648509233ca005d3adf716160be6ef
Aelexx <madisonguitar@yahoo.com>
2025-07-29 21:19:05 +0300

    zxczxcz

--- src/pages/Rio-prices.js
+++ src/pages/Rio-prices.js
@@ -3,7 +3,7 @@ function Rioprices(){
     const lat = 52.52; // Example latitude
     const lon = 13.405; // Example longitude
     const apiKey = 'eb9c24f68ed54d3b98d180818252907'; // Replace with your actual API key
-    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=eb9c24f68ed54d3b98d180818252907&q=Rio de janeiro&aqi=no`;
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=eb9c24f68ed54d3b98d180818252907&q=Rio de janeiro&aqi=no`;
 
     fetch(apiUrl)
       .then(response => {
