147eea8f2366faef22bdbe7491e3eee148de0e0b
null <null>
2025-07-30 15:49:14 +0300

    qwe

--- src/pages/Rio-prices.js
+++ src/pages/Rio-prices.js
@@ -4,7 +4,7 @@ function Rioprices(){
     const lon = 13.405; // Example longitude
     const apiKey = 'eb9c24f68ed54d3b98d180818252907'; // Replace with your actual API key
     const apiUrl = `https://api.weatherapi.com/v1/current.json?key=eb9c24f68ed54d3b98d180818252907&q=Rio de janeiro&aqi=no`;
-
+    const targetDiv = document.getElementById('div-fetch');
     fetch(apiUrl)
       .then(response => {
         if (!response.ok) {
@@ -13,7 +13,11 @@ function Rioprices(){
         return response.json();
       })
       .then(data => {
-        console.log('Weather data:', data);
+        
+        <div id="div-fetch">
+
+        </div>
+        //console.log('Weather data:', data);
         // Process and display the data on your site
       })
       .catch(error => {
