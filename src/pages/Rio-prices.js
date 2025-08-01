138377a0c2cd4d129fdcd60d426944b5335aca31
null <null>
2025-08-01 20:43:44 +0300

    xcvk

--- src/pages/Rio-prices.js
+++ src/pages/Rio-prices.js
@@ -1,8 +1,8 @@
 function Rioprices(){
 
-    const lat = 52.52; // Example latitude
-    const lon = 13.405; // Example longitude
-    const apiKey = 'eb9c24f68ed54d3b98d180818252907'; // Replace with your actual API key
+    // const lat = 52.52; // Example latitude
+    // const lon = 13.405; // Example longitude
+    //const apiKey = 'eb9c24f68ed54d3b98d180818252907'; // Replace with your actual API key
     const apiUrl = `https://api.weatherapi.com/v1/current.json?key=eb9c24f68ed54d3b98d180818252907&q=Rio de janeiro&aqi=no`;
     const dataContainer = document.getElementById("data-container");
     fetch(apiUrl)
